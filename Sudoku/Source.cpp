5d5a777ba7fc3cb451a594802eec83a7d54c8ed2
Monsotya <sonya20033009@gmail.com>
2021-04-24 18:06:54 +0300

    Added names for functions

---
+++ Sudoku/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int complexity();
+int** create_sudoku1();
+int** create_sudoku2(int ind);
+void out(int table[8][8]);
+//int check(int table[8][8]);
+
+int main()
+{
+}
+int complexity() {
+
+}
+int** create_sudoku1() {
+
+}
+int** create_sudoku2(int ind) {
+
+}
+void out(int table[8][8]) {
+
+}
