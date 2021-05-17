762788d72280926a786c4dabfcd466faeef4e9ca
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2021-05-17 10:26:38 +0300

    Update PrintingSudoku.cpp

--- Sudoku/PrintingSudoku.cpp
+++ Sudoku/PrintingSudoku.cpp
@@ -1,4 +1,56 @@
 #include "header.h"
-void Sudoku::Out() {
-
+void Sudoku::Out(){
+	cout << "  y";
+	int row = 0;
+	int col;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cout.width(5);
+			cout << row++;
+		}
+		cout << "    ";
+	}
+	cout << '\n';
+	row = 0;
+	for(int i=0;i<3;i++){
+		if(!i){
+			cout << "x ";
+		}else{
+			cout << "  ";
+		}
+		cout.width(61);
+		cout.fill('-');
+		cout << '-' << '\n';
+		for(int a=0;a<3;a++){
+			col = 0;
+			cout << row << " ||";
+			cout.fill(' ');
+			for(int r=0;r<3;r++){
+				cout.width(4);
+				if(matrix[row][col] == 0){
+					cout << '-';
+				}else{
+					cout << matrix[row][col];
+				}
+				col++;
+				for(int k=0;k<2;k++){
+					cout.width(5);
+					if(matrix[row][col] == 0){
+						cout << '-';
+					}else{
+						cout << matrix[row][col];
+					}
+					col++;
+				}
+				cout.width(5);
+				cout << '|';
+			}
+			row++;
+			cout << "|\n";
+		}
+	}
+	cout << "  ";
+	cout.width(61);
+	cout.fill('-');
+	cout << '-' << '\n';
 }
