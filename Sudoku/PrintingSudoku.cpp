428a265005a0bd62aa28bc281484646891764a40
Monsotya <sonya20033009@gmail.com>
2021-05-23 20:04:02 +0300

    Update PrintingSudoku.cpp

--- Sudoku/PrintingSudoku.cpp
+++ Sudoku/PrintingSudoku.cpp
@@ -3,47 +3,49 @@ void Sudoku::PrintingSudoku() {
 	cout << "  y ";
 	int row = 0;
 	int col;
-	for(int i=0;i<SizeSmall;i++){
-		for(int j=0;j<SizeSmall;j++){
-			cout.width(SizeWid);
+	for (int i = 0;i < SIZESMALL;i++) {
+		for (int j = 0;j < SIZESMALL;j++) {
+			cout.width(SIZEWID);
 			cout << row++;
 		}
-		cout.width(SizeWid);
+		cout.width(SIZEWID);
 		cout << " ";
 	}
 	cout << '\n';
 	row = 0;
-	for(int i=0;i<SizeSmall;i++){
-		if(!i){
+	for (int i = 0;i < SIZESMALL;i++) {
+		if (!i) {
 			cout << "x ";
-		}else{
+		}
+		else {
 			cout << "  ";
 		}
-		cout.width(Line);
+		cout.width(LINE);
 		cout.fill('-');
 		cout << '-' << '\n';
-		for(int a=0;a<SizeSmall;a++){
+		for (int a = 0;a < SIZESMALL;a++) {
 			col = 0;
 			cout << row << " ||";
 			cout.fill(' ');
-			for(int r=0;r<SizeSmall;r++){
-				/*cout.width(SizeWid-1);
+			for (int r = 0;r < SIZESMALL;r++) {
+				/*cout.width(SIZESWID-1);
 				if(matrix[row][col] == 0){
 					cout << '-';
 				}else{
 					cout << matrix[row][col];
 				}
 				col++;*/
-				for(int k=0;k<SizeSmall;k++){
-					cout.width(SizeWid);
-					if(matrix[row][col] == 0){
+				for (int k = 0;k < SIZESMALL;k++) {
+					cout.width(SIZEWID);
+					if (matrix[row][col] == 0) {
 						cout << '-';
-					}else{
+					}
+					else {
 						cout << matrix[row][col];
 					}
 					col++;
 				}
-				cout.width(SizeWid);
+				cout.width(SIZEWID);
 				cout << '|';
 			}
 			row++;
@@ -51,7 +53,7 @@ void Sudoku::PrintingSudoku() {
 		}
 	}
 	cout << "  ";
-	cout.width(Line);
+	cout.width(LINE);
 	cout.fill('-');
 	cout << '-' << '\n';
 	cout.fill(' ');
